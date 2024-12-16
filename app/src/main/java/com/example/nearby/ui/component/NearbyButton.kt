6699b47520838aa8868d293af3f126b382655b21
Lucas Barbosa Antonelli <lucaslba2105@gmail.com>
2024-12-15 21:49:29 -0300

    conclusao-desafio

---
+++ app/src/main/java/com/example/nearby/ui/component/NearbyButton.kt
@@ -0,0 +1,25 @@
+package com.example.nearby.ui.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NearbyButton(modifier: Modifier = Modifier,
+                 text: String? = null,
+                 @DrawableRes iconRes: Int? = null,
+                 onClick: () -> Unit
+) {
+    Button(modifier = Modifier, onClick = onClick) {
+
+    }
+}
+
+@Preview
+@Composable
+private fun NearbyButtonPreview(){
+    NearbyButton(modifier = Modifier.fillMaxWidth(), text = "Confirmar", iconRes = null ){}
+}
